4ab38eed8cc0791b9b639cad4e62db65a7601b8f
Jumanaafra <148995235+Jumanaafra@users.noreply.github.com>
2025-11-16 11:21:48 +0530

    Update GuideLoginScreen.tsx

--- AuraVision2.0/screens/GuideLoginScreen/GuideLoginScreen.tsx
+++ AuraVision2.0/screens/GuideLoginScreen/GuideLoginScreen.tsx
@@ -39,7 +39,7 @@ export const GuideLoginScreen: React.FC<GuideLoginScreenProps> = ({ setPage }) =
 
     try {
       // Backend login API
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://b-smart-glass-aura-vision.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
@@ -146,3 +146,4 @@ export const GuideLoginScreen: React.FC<GuideLoginScreenProps> = ({ setPage }) =
     </div>
   );
 };
+
