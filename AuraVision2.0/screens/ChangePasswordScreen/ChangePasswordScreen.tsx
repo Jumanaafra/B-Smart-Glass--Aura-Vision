15a05dfd8b8ace7babb2694bc72f7db23fb56a53
Jumanaafra <148995235+Jumanaafra@users.noreply.github.com>
2025-11-16 11:30:38 +0530

    Update ChangePasswordScreen.tsx

--- AuraVision2.0/screens/ChangePasswordScreen/ChangePasswordScreen.tsx
+++ AuraVision2.0/screens/ChangePasswordScreen/ChangePasswordScreen.tsx
@@ -47,7 +47,7 @@ export const ChangePasswordScreen: React.FC<ChangePasswordScreenProps> = ({ setP
     setIsLoading(true);
 
     try {
-        const response = await fetch('http://localhost:5000/api/auth/change-password', {
+        const response = await fetch('https://b-smart-glass-aura-vision.onrender.com/api/auth/change-password', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -124,4 +124,5 @@ export const ChangePasswordScreen: React.FC<ChangePasswordScreenProps> = ({ setP
       </div>
     </div>
   );
+
 };
